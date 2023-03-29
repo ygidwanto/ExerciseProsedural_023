29cc4db2e70f4add6ed038a5d25d2474dddd81d7
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-03-29 14:15:08 +0700

    memperbaiki

--- ExerciseProsedural_023/ExerciseProsedural_023.cpp
+++ ExerciseProsedural_023/ExerciseProsedural_023.cpp
@@ -1,14 +1,2 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
-
-// Fungsi untuk menentukan status kandidat berdasarkan nilai
-string getStatus(int nilaiMatematika, int nilaiRataRata) {
-    if (nilaiRataRata >= 70 || nilaiMatematika > 80) {
-        return "Diterima";
-    }
-    else {
-        return "Ditolak";
-    }
-}
