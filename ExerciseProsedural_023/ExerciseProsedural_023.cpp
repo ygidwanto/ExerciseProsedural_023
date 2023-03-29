829a34ffd6c7a559ac63ebe87b89054151d5d6ad
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-03-29 14:45:03 +0700

    memunculkan status kelulusannya

--- ExerciseProsedural_023/ExerciseProsedural_023.cpp
+++ ExerciseProsedural_023/ExerciseProsedural_023.cpp
@@ -6,7 +6,7 @@ double rerata(double a, double b)
 	return (a + b) / 20;
 }
 string status(double d)
-{
+{  
 	if (d >= 70)
 		return "diterima";
 	else
@@ -18,4 +18,20 @@ string status2(double r, double n)
 		return "ditolak";
 	else
 		return "ditolak";
+} 
+int main()
+{
+	double nilM, nilB;
+
+	cout << "masukkan nilai matematika B : ";
+	cin >> nilM;
+	cout << "masukkan nilai bahasa inggris A : ";
+	cin >> nilB;
+
+	//rerata = (nilM + nilB) / 20;
+	 //rata = rerata (nilM, nilB);
+	 //st = status (rata);
+	//st = status (rerata (nilM, nilB));
+	cout << "status kelulusannya = " << status(rerata(nilM, nilB)) << endl;
+	cout << "status kelulusannya = " << status2(rerata(nilM, nilB), nilM);
 }
