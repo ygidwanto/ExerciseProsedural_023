079f317e3168b3194938917b69977575dd4ab3bc
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-03-29 14:07:02 +0700

    memasukan nilai rata-rata

--- ExerciseProsedural_023/ExerciseProsedural_023.cpp
+++ ExerciseProsedural_023/ExerciseProsedural_023.cpp
@@ -3,3 +3,14 @@
 
 using namespace std;
 
+int main()
+{                       // Fungsi untuk menentukan status kandidat berdasarkan nilai
+	string getstatus(int nilaiMatematika, int nilaiRerata) {
+		if (nilaiRata - Rata >= 70 || nilaiMatematika > 80) {
+			return "Diterima";
+		}
+		else {
+			return "Ditolak";
+		}
+}
+}
